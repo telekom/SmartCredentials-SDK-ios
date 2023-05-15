936fd85c4595acd46d34c3c2d8173243726a2d28
George Cristian Cuciureanu <georgecristian.cuciureanu@endava.com>
2023-05-15 11:59:29 +0300

    -Changed enum Endpoints

--- SmartCredentials/IdentityProvider/IdentityProvider/Network/Requests.swift
+++ SmartCredentials/IdentityProvider/IdentityProvider/Network/Requests.swift
@@ -24,13 +24,13 @@ class Requests {
     }
     
     public func getBearerTokenRequest(accessToken: String, clientId: String, scope: String) {
-        let url = URL(string: "https://lbl-partmgmr.superdtaglb.cf/bearer-token-hackathon")!
+        let bearerTokenURL = Endpoints.getBearerTokenEndpoint.url!
         let requestBody = BearerTokenRequestBody(accessToken: accessToken, bundleId: Bundle.main.bundleIdentifier, packageName: nil, clientId: clientId,scope: scope)
         do {
             let encoder = JSONEncoder()
             let data = try encoder.encode(requestBody)
             let jsonString = String(data: data, encoding: .utf8)
-            networkManager.postRequest(url: url , body: data ) { result in
+            networkManager.postRequest(url: bearerTokenURL , body: data ) { result in
                 switch result {
                 case .success(let data):
                     let bearerToken = String(decoding: data, as: UTF8.self)
@@ -59,5 +59,36 @@ enum Endpoint {
         case .baseURL(let url):
             return url
         }
+    }
+}
+
+enum Endpoints {
+    
+    case getAccessTokenEndpoint
+    case getBearerTokenEndpoint
+    case credentials
+    
+    var baseUrl: String {
+        return "https://lbl-partmgmr.superdtaglb.cf"
+    }
+    var path:String {
+        switch self {
+        case .getAccessTokenEndpoint:
+            return "/access-token"
+        case .getBearerTokenEndpoint:
+            return "/bearer-token-hackathon"
+        case .credentials:
+            return "/Odysee-45930b82-5f64-412f-9993-3456c4c61bbc"
+        }
+    }
+    
+    var endpoint: URL? {
+        return URL( string: path)
+    }
+    var url: URL? {
+        return URL( string: baseUrl+path)
+    }
+    var credentials: URL? {
+        return URL( string: path)
     }
 }
